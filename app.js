cb9cbd2f81409643677a9a984f8cd59bd3669ea9
ishimwepati <wazacode@gmail.com>
2023-07-10 18:57:47 +0300

    app.js modified

---
+++ app.js
@@ -0,0 +1,54 @@
+var books = [];
+
+function Bookadded() {
+    var title = document.getElementById("titleInput").value;
+    var author = document.getElementById("authorInput").value;
+    
+    var book = { title: title, author: author };
+    books.push(book);
+    
+    displayBooks();
+    
+    // this is just oto clear the inputs after
+    document.getElementById("titleInput").value = "";
+    document.getElementById("authorInput").value = "";
+    }
+
+    // Code to remove from the index
+function removeBook(index) {
+        books.splice(index, 1);
+        displayBooks();
+    }
+
+    // Function to displauy
+function displayBooks() {
+        var bookContainer = document.getElementById("bookContainer");
+        bookContainer.innerHTML = "";
+
+        books.forEach(function(book, index) {
+            var bookDiv = document.createElement("div");
+            bookDiv.className = "book";
+
+            var titleElement = document.createElement("p");
+            titleElement.className = "title";
+            titleElement.textContent = book.title;
+            bookDiv.appendChild(titleElement);
+
+            var authorElement = document.createElement("p");
+            authorElement.textContent = "Author: " + book.author;
+            bookDiv.appendChild(authorElement);
+
+            var removeButton = document.createElement("button");
+            removeButton.className = "remove-button";
+            removeButton.textContent = "Remove this Book";
+            removeButton.addEventListener("click", function() {
+                removeBook(index);
+            });
+            bookDiv.appendChild(removeButton);
+
+            bookContainer.appendChild(bookDiv);
+
+            var line = document.createElement("hr");
+            bookContainer.appendChild(line);
+        });
+    }
