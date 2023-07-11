27f4c64f4e5dfc578825912dbec4327b82d1c4c7
Armynerh <aimenah@outlook.com>
2023-07-11 10:03:56 +0100

    solved a merge conflict

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-var books = [];
+let books = [];
 
-window.onload = function(){
-    var storedBooks = localStorage.getItem('books');
-    if (storedBooks){
-        books = JSON.parse(storedBooks);
-        displayBooks()
-    }
-};
+/* Create function that runs when document loads to populate books array/collection with values from local storage
+*/
+
+  
+
+// Function to display
+function displayBooks() {
+  const bookContainer = document.getElementById('bookContainer');
+  bookContainer.innerHTML = '';
+
+  // Code to remove from the index
+  function removeBook(index) {
+    // remove book from collection using filter by id
+    books.splice(index, 1);
+    // remove book from loclStorage using filter by id
+    displayBooks();
+  }
+
+  books.forEach((book, index) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book';
+
+    const titleElement = document.createElement('p');
+    titleElement.className = 'title';
+    titleElement.textContent = book.title;
+    bookDiv.appendChild(titleElement);
+
+    const authorElement = document.createElement('p');
+    authorElement.textContent = `Author: ${book.author}`;
+    bookDiv.appendChild(authorElement);
+
+    const removeButton = document.createElement('button');
+    removeButton.className = 'remove-button';
+    removeButton.textContent = 'Remove this Book';
+    removeButton.addEventListener('click', () => {
+      removeBook(index);
+    });
+    bookDiv.appendChild(removeButton);
+
+    bookContainer.appendChild(bookDiv);
+
+    const line = document.createElement('hr');
+    bookContainer.appendChild(line);
+  });
+}
+
 
 function bookadded() {
-    var title = document.getElementById("titleInput").value;
-    var author = document.getElementById("authorInput").value;
-    
-    var book = { title: title, author: author };
-    books.push(book);
-    
-    displayBooks();
-    
-    // this is just oto clear the inputs after
-    document.getElementById("titleInput").value = "";
-    document.getElementById("authorInput").value = "";
+  const title = document.getElementById('titleInput').value;
+  const author = document.getElementById('authorInput').value;
+  const id = `${books.length - 1}-${title}`;
 
-    // to save books storage
-    localStorage.setItem('books', JSON.stringify(books));
+  const book = { title, author, id };
+  books.push(book);
+  // Implement ME: Push to local storage
+  savedFormData()
+  displayBooks();
+  
 
-    }
+  // this is just oto clear the inputs after
+  document.getElementById('titleInput').value = '';
+  document.getElementById('authorInput').value = '';
+}
 
-    // Code to remove from the index
-function removeBook(index) {
-        books.splice(index, 1);
-        displayBooks();
-
-     localStorage.setItem('books', JSON.stringify(books));
-    }
-
-    // Function to displauy
-function displayBooks() {
-        var bookContainer = document.getElementById("bookContainer");
-        bookContainer.innerHTML = "";
-
-        books.forEach(function(book, index) {
-            var bookDiv = document.createElement("div");
-            bookDiv.className = "book";
-
-            var titleElement = document.createElement("p");
-            titleElement.className = "title";
-            titleElement.textContent = "Title : " + book.title;
-            bookDiv.appendChild(titleElement);
-
-            var authorElement = document.createElement("p");
-            authorElement.textContent = "Author : " + book.author;
-            bookDiv.appendChild(authorElement);
-
-            var removeButton = document.createElement("button");
-            removeButton.className = "remove-button";
-            removeButton.textContent = "Remove this Book";
-            removeButton.addEventListener("click", function() {
-                removeBook(index);
-            });
-            bookDiv.appendChild(removeButton);
-
-            bookContainer.appendChild(bookDiv);
-
-            var line = document.createElement("hr");
-            bookContainer.appendChild(line);
-        });
-    }
+displayBooks();
+bookadded();
