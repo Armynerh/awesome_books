7bf3281e4d358f13c94ea88891029d8467c6fdb5
ishimwepati <wazacode@gmail.com>
2023-07-11 12:01:13 +0300

    Update

--- app.js
+++ app.js
@@ -1,68 +1,67 @@
-var books = [];
+let books = [];
 
-window.onload = function(){
-    var storedBooks = localStorage.getItem('books');
-    if (storedBooks){
-        books = JSON.parse(storedBooks);
-        displayBooks()
-    }
+// Code to remove from the index
+function removeBook(index) {
+  books.splice(index, 1);
+  displayBooks();
+
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+// Function to displauy
+function displayBooks() {
+  const bookContainer = document.getElementById('bookContainer');
+  bookContainer.innerHTML = '';
+
+  books.forEach((book, index) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book';
+
+    const titleElement = document.createElement('p');
+    titleElement.className = 'title';
+    titleElement.textContent = `Title : ${book.title}`;
+    bookDiv.appendChild(titleElement);
+
+    const authorElement = document.createElement('p');
+    authorElement.textContent = `Author : ${book.author}`;
+    bookDiv.appendChild(authorElement);
+
+    const removeButton = document.createElement('button');
+    removeButton.className = 'remove-button';
+    removeButton.textContent = 'Remove this Book';
+    removeButton.addEventListener('click', () => {
+      removeBook(index);
+    });
+    bookDiv.appendChild(removeButton);
+
+    bookContainer.appendChild(bookDiv);
+
+    const line = document.createElement('hr');
+    bookContainer.appendChild(line);
+  });
+}
+
+window.onload = function () {
+  const storedBooks = localStorage.getItem('books');
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+    displayBooks();
+  }
 };
 
 function bookadded() {
-    var title = document.getElementById("titleInput").value;
-    var author = document.getElementById("authorInput").value;
-    
-    var book = { title: title, author: author };
-    books.push(book);
-    
-    displayBooks();
-    
-    // this is just oto clear the inputs after
-    document.getElementById("titleInput").value = "";
-    document.getElementById("authorInput").value = "";
+  const title = document.getElementById('titleInput').value;
+  const author = document.getElementById('authorInput').value;
 
-    // to save books storage
-    localStorage.setItem('books', JSON.stringify(books));
+  const book = { title, author };
+  books.push(book);
 
-    }
+  displayBooks();
 
-    // Code to remove from the index
-function removeBook(index) {
-        books.splice(index, 1);
-        displayBooks();
+  // this is just oto clear the inputs after
+  document.getElementById('titleInput').value = '';
+  document.getElementById('authorInput').value = '';
 
-     localStorage.setItem('books', JSON.stringify(books));
-    }
-
-    // Function to displauy
-function displayBooks() {
-        var bookContainer = document.getElementById("bookContainer");
-        bookContainer.innerHTML = "";
-
-        books.forEach(function(book, index) {
-            var bookDiv = document.createElement("div");
-            bookDiv.className = "book";
-
-            var titleElement = document.createElement("p");
-            titleElement.className = "title";
-            titleElement.textContent = "Title : " + book.title;
-            bookDiv.appendChild(titleElement);
-
-            var authorElement = document.createElement("p");
-            authorElement.textContent = "Author : " + book.author;
-            bookDiv.appendChild(authorElement);
-
-            var removeButton = document.createElement("button");
-            removeButton.className = "remove-button";
-            removeButton.textContent = "Remove this Book";
-            removeButton.addEventListener("click", function() {
-                removeBook(index);
-            });
-            bookDiv.appendChild(removeButton);
-
-            bookContainer.appendChild(bookDiv);
-
-            var line = document.createElement("hr");
-            bookContainer.appendChild(line);
-        });
-    }
+  // to save books storage
+  localStorage.setItem('books', JSON.stringify(books));
+}
